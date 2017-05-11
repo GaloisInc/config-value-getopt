b6636c57981909fb9ab588ee1e5cacd1057d8f2c
Eric Mertens <emertens@galois.com>
2017-05-10 17:23:11 -0700

    Update for new config-value version

--- src/Config/GetOpt.hs
+++ src/Config/GetOpt.hs
@@ -21,14 +21,14 @@ import qualified Data.Text as Text
 -- To omit an option entirely, set it to `no` or remove it from the file.
 --
 -- Options can be provided as decimal literals or string literals.
-configValueGetOpt :: [OptDescr a] -> Value -> ([a], [String])
-configValueGetOpt descrs (Sections sections) =
+configValueGetOpt :: [OptDescr a] -> Value p -> ([a], [String])
+configValueGetOpt descrs (Sections _ sections) =
   swap (partitionEithers (mapMaybe (sectionToOpts descrs) sections))
 configValueGetOpt _ _ = ([],["invalid configuration value"])
 
-sectionToOpts :: [OptDescr a] -> Section -> Maybe (Either String a)
-sectionToOpts _ (Section _ (Atom "no")) = Nothing
-sectionToOpts descrs (Section k v) = Just $
+sectionToOpts :: [OptDescr a] -> Section p -> Maybe (Either String a)
+sectionToOpts _ (Section _ _ (Atom _ "no")) = Nothing
+sectionToOpts descrs (Section _ k v) = Just $
   let k' = Text.unpack k in
   case lookupOption k' descrs of
     Nothing -> Left (k' ++ ": unknown option")
@@ -38,27 +38,27 @@ sectionToOpts descrs (Section k v) = Just $
         OptArg f _ -> optArg k' v f
         ReqArg f _ -> reqArg k' v f
 
-noArg :: String -> Value -> a -> Either String a
-noArg _ (Atom "yes") x = Right x
+noArg :: String -> Value p -> a -> Either String a
+noArg _ (Atom _ "yes") x = Right x
 noArg k _ _ = Left (k ++ ": invalid parameter, expected `yes` or `no`")
 
-optArg :: String -> Value -> (Maybe String -> a) -> Either String a
-optArg _ (Atom "yes") f = Right (f Nothing)
+optArg :: String -> Value p -> (Maybe String -> a) -> Either String a
+optArg _ (Atom _ "yes") f = Right (f Nothing)
 optArg k v f =
   case valueString v of
     Just x -> Right (f (Just x))
     Nothing -> Left (k ++ ": invalid parameter")
 
-reqArg :: String -> Value -> (String -> a) -> Either String a
+reqArg :: String -> Value p -> (String -> a) -> Either String a
 reqArg k v f =
   case valueString v of
     Just x -> Right (f x)
     Nothing -> Left (k ++ ": invalid parameter")
 
-valueString              :: Value -> Maybe String
-valueString (Text t)      = Just (Text.unpack t)
-valueString (Number 10 n) = Just (show n)
-valueString _             = Nothing
+valueString                :: Value p -> Maybe String
+valueString (Text _ t)      = Just (Text.unpack t)
+valueString (Number _ 10 n) = Just (show n)
+valueString _               = Nothing
 
 lookupOption :: String -> [OptDescr a] -> Maybe (OptDescr a)
 lookupOption name = find $ \o -> name `elem` optionLongNames o
